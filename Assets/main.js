4c4bcd4cbde04aa28a980bff2fc8407bcc7ab346
Connie Tran <connietrandev@gmail.com>
2020-04-02 19:39:46 -0400

    Small edits

--- Assets/main.js
+++ Assets/main.js
@@ -1,47 +1,47 @@
 // Initialize empty array for saved events
-var events = JSON.parse(localStorage.getItem("events")) || [];
-var checkStorage = localStorage.getItem("events");
+var events = JSON.parse(localStorage.getItem('events')) || [];
+var checkStorage = localStorage.getItem('events');
 var startTime = 9;
 var currentTime = moment();
 
 
 // Display current date in jumbotron
-$("#currentDay").text(currentTime.format("llll"));
+$('#currentDay').text(currentTime.format('llll'));
 
 
 // Generate time blocks for 9 am to 5 pm
 for (let index = 0; index < 9; index++) {
 
     // Create row
-    var divRow = $("<div class='row'>");
-    divRow.attr("data-id", index);
-    $(".container").append(divRow);
+    var divRow = $('<div class="row">');
+    divRow.attr('data-id', index);
+    $('.container').append(divRow);
 
     if (checkStorage === null) {
 
         // Create objects in events array with index as id for local storage
         events.push({ eventId: index });
         console.log(events);
-        localStorage.setItem("events", JSON.stringify(events));
+        localStorage.setItem('events', JSON.stringify(events));
         
     };
 
 
     // Populate first column with time
     var tableTime = $('<div class="hour col-1">');
-    var hour = moment(startTime, "h").format("LT");
+    var hour = moment(startTime, 'h').format('LT');
     tableTime.text(hour);
     divRow.append(tableTime);
 
     // Create Description div and <textarea>s for events
     var eventDesc = $('<div class="description col-10">');
-    eventDesc.attr("data-time", startTime);
+    eventDesc.attr('data-time', startTime);
 
     setDescClass();
 
     var eventTextArea = $('<textarea cols="65" rows="3"></textarea>');
     eventDesc.html(eventTextArea);
-    eventTextArea.attr("data-id", index);
+    eventTextArea.attr('data-id', index);
     divRow.append(eventDesc);
 
     // Get event description out of local storage to display in text event
@@ -51,9 +51,9 @@ for (let index = 0; index < 9; index++) {
     // Create save buttons
     var saveDiv = $('<div class="saveBtn col-1">');
     divRow.append(saveDiv);
-    var saveBtn = $("<i class='far fa-save fa-md'></i>");
+    var saveBtn = $('<i class="far fa-save fa-md"></i>');
     saveDiv.html(saveBtn);
-    saveBtn.attr("data-id", index);
+    saveBtn.attr('data-id', index);
 
     startTime++;
 
@@ -67,18 +67,18 @@ function setDescClass() {
 
     // Set class for Description div based on past/present/future
     if (moment(startTime).isBefore(currentTime)) {
-        eventDesc.addClass("past");
-        console.log("past");
+        eventDesc.addClass('past');
+        console.log('past');
     };
 
     if (moment(startTime).isSame(currentTime)) {
-        eventDesc.addClass("present");
-        console.log("present");
+        eventDesc.addClass('present');
+        console.log('present');
     };
 
     if (moment(startTime).isAfter(currentTime)) {
-        eventDesc.addClass("future");
-        console.log("future");
+        eventDesc.addClass('future');
+        console.log('future');
     };
 
 };
@@ -87,10 +87,10 @@ function setDescClass() {
 
 
 // Add event listener to save button
-$(".far").on("click", function () {
+$('.far').on('click', function () {
 
-    // Get data-id of saveBtn which is set to index in array
-    var dataId = $(this).attr("data-id");
+    // Get data-id of save click which is set to index in array
+    var dataId = $(this).attr('data-id');
     console.log("The data-id of clicked item is: " + dataId);
 
     // Take textarea value with that currentId and...
@@ -101,7 +101,7 @@ $(".far").on("click", function () {
     events[dataId].description = eventText;
     console.log(events);
 
-    localStorage.setItem("events", JSON.stringify(events));
+    localStorage.setItem('events', JSON.stringify(events));
 
     console.log("Description saved for id: " + dataId);
 
